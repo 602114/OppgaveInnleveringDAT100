3c35d4ee1812abbca8fa8dc8761acab6281e81ab
602114 <89074496+602114@users.noreply.github.com>
2021-09-10 23:46:23 +0200

    La til metode og endret feilmelding

--- src/no/hvl/dat100/Karakter.java
+++ src/no/hvl/dat100/Karakter.java
@@ -7,45 +7,38 @@ public class Karakter {
 	public static void main(String[] args) {
 
 		int poeng = 0;
-		String karakter;
 		
-		for (int elever=1; elever<=10; elever++) {
-			String skrivPoeng = showInputDialog("Poeng: ");
-			poeng = Integer.parseInt(skrivPoeng);
-		
-		
-			if (poeng <= 100 && poeng >= 90) {
-				karakter = "A";
-				System.out.println(karakter);
+		for (int elever=1; elever<=10; elever++) { // Teller antall forsøk som antall elever
+				String skrivPoeng = showInputDialog("Poeng: ");
+				poeng = Integer.parseInt(skrivPoeng);
 			
-			} else if (poeng <= 89 && poeng >= 80){
-				karakter = "B";
-				System.out.println(karakter);
-			
-			} else if (poeng <= 79 && poeng >= 60){
-				karakter = "C";
-				System.out.println(karakter);
-			
-			} else if (poeng <= 59 && poeng >= 50){
-				karakter = "D";
-				System.out.println(karakter);
-			
-			} else if (poeng <= 49 && poeng >= 40){
-				karakter = "E";
-				System.out.println(karakter);
-				showMessageDialog(null, karakter);
-			
-			} else if (poeng <= 39 && poeng >= 0){
-				karakter = "F";
-				System.out.println(karakter);
-			
-			} else if (poeng > 100 || poeng < 0){
-				String ugyldig = "Ugyldig karakter, skriv inn på nytt";
-				showMessageDialog (null, ugyldig);
-				elever--;
+			if (poeng > 100 || poeng < 0) { // Dette er for å fange opp om tallet er ugyldig
+				showMessageDialog (null, "Ugyldig karakter, skriv inn på nytt");
+				elever--; // Trenger denne for at det ugyldige forsøket ikke skal telle som en elev
+				continue; // Gå videre til neste loop, i dette tilfellet til for-loopen
 			}
-		}
-		
-		
+			
+			showMessageDialog (null, "Karakteren for elev nr. " + elever + " er: " + karakter(poeng)); // Denne setningen kjøres etter man har skrevet inn en valid poengsum
+		}	
 	}
+	
+	// Dette er metoden som finner den riktige karakteren for poengsummen
+	public static String karakter(int poeng) {
+		if (poeng <= 100 && poeng >= 90) {
+			return "A";		
+		} else if (poeng <= 89 && poeng >= 80) {
+			return "B";
+		} else if (poeng <= 79 && poeng >= 60) {
+			return "C";	
+		} else if (poeng <= 59 && poeng >= 50) {
+			return "D";	
+		} else if (poeng <= 49 && poeng >= 40) {
+			return "E";	
+		} else if (poeng <= 39 && poeng >= 0) {
+			return "F";	
+		} 
+		
+		return ""; // Grei å ha i tilfelle
+	}
+	
 }
